9342cc12324b8e3d6b462c827f311f8e3b4292ff
ec49ce <ec49ce@outlook.com>
2022-09-22 19:03:43 +0300

    BashScripts-L11-print-odd

--- BashScripts/File_generator2-odd-even.sh
+++ BashScripts/File_generator2-odd-even.sh
@@ -18,13 +18,15 @@ function getRandomNumber() {
 ##################
 # gen files in dir
 ##################
-function genFiles(){
+function genFiles
+{
 
-	local flsCnt=$(getRandomNumber)
+	local flsCnt=4+$(getRandomNumber)
 
         for (( i=1; i<=$flsCnt; i++))
                 do
-                echo -n > $1"/fileName"$2-$i
+                local flName=("fileName"$i)
+                echo -n > $1/$flName
                 done
 }
 
@@ -36,7 +38,7 @@ function genDirs
 	if [[ ! -d $1/myDir$3-$2 ]]
 	then
         mkdir $1/myDir$3-$2
-	genFiles $1/myDir$3-$2 $3
+	    #genFiles $1/myDir$3-$2 $3
 	fi
 }
 
@@ -53,17 +55,24 @@ ParentDir=$1
 # else remove and create new one
 if  [ ! -d ${ParentDir} ]
 then
+    echo "create parent dir"
     mkdir -p ${ParentDir}
 else
+    echo "clean parent dir"
     rm -r ${ParentDir}
+
+    echo "create parent dir"
     mkdir -p ${ParentDir}
 fi
 
 #
 # generating Tree and fill it with files
 #
+echo "gen tree"
+
 while [[ $CurDepth -le MaxDepth ]]
 	do
+        echo "CurDepth=$CurDepth"
 
 		find ${ParentDir}"/" -mindepth $(( $CurDepth - 1 )) -type d -print0 | while read -d $'\0' dir
 			do
@@ -81,3 +90,56 @@ while [[ $CurDepth -le MaxDepth ]]
 
 	done
 
+
+echo "gen files"
+
+find ${ParentDir}"/" -type d -print0 | while read -d $'\0' dir
+    do
+            genFiles $dir
+    done
+
+
+
+echo "fill files with odd/even"
+
+let totalFilesCnt=102
+cc=0
+
+find ${ParentDir}"/" -type d -print0 | while read -d $'\0' dir 
+    do
+           
+           #echo $dir
+
+           filesInDirCnt=0 
+           
+           find $dir -maxdepth 1 -type f -print0 | while read -d $'\0' filename 
+            do
+                
+                let totalFilesCnt++
+
+                let filesInDirCnt++
+                
+                echo "$totalFilesCnt/$filesInDirCnt"
+
+                if [ $(($filesInDirCnt%2)) -eq 0 ];
+                    then 
+                        echo "even - $filename" > $filename
+                    else
+                        echo "odd - $filename" > $filename
+                fi
+
+            done
+            
+            cc=$(($cc+1))
+            
+            if [ $cc -eq 3 ] 
+                then 
+                    break 
+            fi
+
+            echo
+
+    done
+
+echo 
+echo "ddddd "$totalFilesCnt
---
+++ BashScripts/Print-matrix.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+clear
+
+#
+#declare array with name "matrix"
+# 
+declare -A matrix
+
+num_rows=6
+num_columns=4
+
+#
+#fill matrix by random values
+#
+for ((i=1;i<=$num_rows;i++)) do
+    for ((j=1;j<=$num_columns;j++)) do
+        matrix[$i,$j]=$(($RANDOM % 99 + 1))
+    done
+done
+
+#format string #1
+f1="%3s|"
+
+#format string #2
+# with 9 sinmbols width
+f2=" %3s"
+
+printf "\n\n\n"
+
+#print header with columns No.
+printf "$f1" ''
+for ((j=1;j<=$num_columns;j++)) do
+    printf "$f2" $j
+done
+printf "\n"
+
+printf "$f1" ''
+for ((j=1;j<=$num_columns;j++)) do
+    printf "$f2" "___"
+done
+printf "\n"
+
+
+#print array 
+for ((i=1;i<=num_rows;i++)) do
+    
+    #print row No 
+    printf "$f1" $i
+
+    for ((j=1;j<=num_columns;j++)) do
+
+        #print value by address [$i,$j] 
+        printf "$f2" ${matrix[$i,$j]}
+    
+    done
+    
+    printf "\n"
+done
+printf "\n\n\n"
+
