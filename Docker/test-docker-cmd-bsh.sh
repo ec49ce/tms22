adf461c8bc74e05404799e431802a24bff345174
ec49ce <ec49ce@outlook.com>
2022-10-17 15:47:42 +0300

    Home work for lessons Docker and Docker compose

--- Docker/test-docker-cmd-bsh.sh
+++ Docker/test-docker-cmd-bsh.sh
@@ -4,13 +4,11 @@ cd /SyncFldr
 # Debian-apache2
 # -------------------------------------------------------------------
 
-cp Debian-apache2.yaml Dockerfile
-
-docker build -t my-deb-apache2:1.0 .
+docker build -t my-deb-apache2:1.0 ./Debian-apache2
 
 docker images
 
-docker run --name mydebapache2 -d -p 80:80 my-deb-apache2:1.0
+docker run --name mydebapache2 -d -p 8080:80 my-deb-apache2:1.0
 
 docker stop mydebapache2
 
@@ -22,16 +20,16 @@ docker image rm my-deb-apache2:1.0
 # CentOS-nginx
 # -------------------------------------------------------------------
 
-cp CentOS-nginx.yaml Dockerfile
 
-docker build -t my-centos-nginx:1.0 .
+docker build -t my-centos-nginx:1.0 ./CentOS-nginx
 
-docker run --name mycentosnginx -d -p 80:80 my-centos-nginx:1.0
+docker run --name mycentosnginx -d -p 8080:80 my-centos-nginx:1.0
 
 docker stop mycentosnginx
 
 docker rm mycentosnginx
 
+docker image rm my-centos-nginx:1.0
 
 # -------------------------------------------------------------------
 #  docker compose
@@ -40,5 +38,7 @@ docker rm mycentosnginx
 cp /SyncFldr/index.html ~/shared-folder-volume/
 
 docker-compose up -d
+
+docker-compose ps
 
 docker-compose down
