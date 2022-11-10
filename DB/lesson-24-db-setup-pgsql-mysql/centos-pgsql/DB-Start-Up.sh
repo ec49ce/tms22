021500c546f2ba2b64d6931b04cb1aa675cf53eb
ec49ce <ec49ce@outlook.com>
2022-11-10 22:05:19 +0300

    Update DB-Start-Up.sh

--- DB/lesson-24-db-setup-pgsql-mysql/centos-pgsql/DB-Start-Up.sh
+++ DB/lesson-24-db-setup-pgsql-mysql/centos-pgsql/DB-Start-Up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-su -l postgres -c /usr/pgsql-15/bin/initdb
-su -l postgres -c "/usr/pgsql-15/bin/pg_ctl -D /var/lib/pgsql/15/data -l /tmp/pg_logfile start"
+/usr/pgsql-15/bin/initdb
+/usr/pgsql-15/bin/pg_ctl -D /var/lib/pgsql/15/data -l /tmp/pg_logfile start
 createdb -U postgres test-db-name
